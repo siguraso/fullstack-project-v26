3d815abcb60fc5901937c0902d738407474dae5c
sigurdandris <sigurd.andris.sorengen@gmail.com>
2026-03-20 10:47:29 +0100

    added maven project skeleton

---
+++ backend/fullstack-project/src/main/java/edu/ntnu/idi/idatt2105/App.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idi.idatt2105;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
