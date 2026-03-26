e4efdbc9e866cece64a6f1a1d000a8ea78d107dc
Edvard Klavenes <edvardklavenes@gmail.com>
2026-03-26 22:52:16 +0100

    start: user controller with REST API

---
+++ backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/user/controller/UserController.java
@@ -0,0 +1,30 @@
+package edu.ntnu.idi.idatt2105.backend.core.user.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ntnu.idi.idatt2105.backend.core.user.dto.CreateUser;
+import edu.ntnu.idi.idatt2105.backend.core.user.dto.UserResponse;
+import edu.ntnu.idi.idatt2105.backend.core.user.service.UserService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public UserResponse createUser(@Valid @RequestBody CreateUser dto) {
+        return service.createUser(dto);
+    }
+
+}
