73a2a0bc51812dc823d854c0e11154865f2e9197
sigurdandris <sigurd.andris.sorengen@gmail.com>
2026-03-26 12:32:19 +0100

    added custom fonts

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sidebar">
+    <h2>title asf</h2>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+
+</template>
+
+<style>
+
+.sidebar {
+  width: 200px;
+  background-color: #ffffff;
+  padding: 20px;
+}
+
+</style>
