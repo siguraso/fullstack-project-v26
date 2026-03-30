52539387fcf2e05c41a57b9a7da691193cd97bc6
Edvard Klavenes <edvardklavenes@gmail.com>
2026-03-30 14:07:57 +0200

    chore: remove unused import

--- backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/log/entity/ComplianceLog.java
+++ backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/log/entity/ComplianceLog.java
@@ -21,7 +21,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
