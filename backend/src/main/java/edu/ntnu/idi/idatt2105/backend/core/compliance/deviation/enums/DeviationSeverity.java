efc51e2e5f34385ca9012e306770ba827c33cf63
Edvard Klavenes <edvardklavenes@gmail.com>
2026-03-30 14:06:09 +0200

    feat: add enmus for deviation entity

---
+++ backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/deviation/enums/DeviationSeverity.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.idatt2105.backend.core.compliance.deviation.enums;
+
+public enum DeviationSeverity {
+    CRITICAL,
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/deviation/enums/DeviationStatus.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idi.idatt2105.backend.core.compliance.deviation.enums;
+
+public enum DeviationStatus {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED
+}
