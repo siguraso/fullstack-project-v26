684ab167fd48b4be7da5188cd020b8e967e1d5d7
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2026-03-31 13:25:26 +0200

    feat(compliance): implement base compliance log structure with DTO, repository, and service

--- backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/log/enums/LogType.java
+++ backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/log/enums/LogType.java
@@ -1,8 +1,9 @@
 package edu.ntnu.idi.idatt2105.backend.core.compliance.log.enums;
 
 public enum LogType {
+    GENERAL,
     TEMPERATURE,
     CLEANING,
-    AGE_VERIFICATION,
-    INCIDENT
+    INCIDENT,
+    OTHER
 }
