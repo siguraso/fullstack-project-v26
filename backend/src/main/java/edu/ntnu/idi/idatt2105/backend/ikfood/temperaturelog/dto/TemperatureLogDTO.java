2a254a6f7eca440a78891cf7f71a6baf5ede88d7
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2026-04-01 20:39:11 +0200

    refactor(temperature-log): simplify TemperatureLogDTO with Lombok and update service to handle deviation creation

--- backend/src/main/java/edu/ntnu/idi/idatt2105/backend/ikfood/temperaturelog/dto/TemperatureLogDTO.java
+++ backend/src/main/java/edu/ntnu/idi/idatt2105/backend/ikfood/temperaturelog/dto/TemperatureLogDTO.java
@@ -1,7 +1,11 @@
 package edu.ntnu.idi.idatt2105.backend.ikfood.temperaturelog.dto;
 
 import edu.ntnu.idi.idatt2105.backend.core.compliance.log.dto.BaseComplianceLogDTO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class TemperatureLogDTO extends BaseComplianceLogDTO {
 
     private Long temperatureZoneId;
@@ -10,54 +14,7 @@ public class TemperatureLogDTO extends BaseComplianceLogDTO {
     private Double upperLimitCelsius;
     private Double temperatureCelsius;
     private String note;
-
-    public Long getTemperatureZoneId() {
-        return temperatureZoneId;
-    }
-
-    public void setTemperatureZoneId(Long temperatureZoneId) {
-        this.temperatureZoneId = temperatureZoneId;
-    }
-
-    public String getTemperatureZoneName() {
-        return temperatureZoneName;
-    }
-
-    public void setTemperatureZoneName(String temperatureZoneName) {
-        this.temperatureZoneName = temperatureZoneName;
-    }
-
-    public Double getLowerLimitCelsius() {
-        return lowerLimitCelsius;
-    }
-
-    public void setLowerLimitCelsius(Double lowerLimitCelsius) {
-        this.lowerLimitCelsius = lowerLimitCelsius;
-    }
-
-    public Double getUpperLimitCelsius() {
-        return upperLimitCelsius;
-    }
-
-    public void setUpperLimitCelsius(Double upperLimitCelsius) {
-        this.upperLimitCelsius = upperLimitCelsius;
-    }
-
-    public Double getTemperatureCelsius() {
-        return temperatureCelsius;
-    }
-
-    public void setTemperatureCelsius(Double temperatureCelsius) {
-        this.temperatureCelsius = temperatureCelsius;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
+    private boolean deviationCreated;
 }
 
 
