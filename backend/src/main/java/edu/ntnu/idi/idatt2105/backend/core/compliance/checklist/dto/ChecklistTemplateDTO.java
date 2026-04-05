5ed7e101cb9935a723ce72dd7df1db173f41ca5f
Edvard Klavenes <edvardklavenes@gmail.com>
2026-04-06 00:27:57 +0200

    feat: adds active boolean

--- backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/checklist/dto/ChecklistTemplateDTO.java
+++ backend/src/main/java/edu/ntnu/idi/idatt2105/backend/core/compliance/checklist/dto/ChecklistTemplateDTO.java
@@ -11,6 +11,7 @@ public class ChecklistTemplateDTO {
     private String name;
     private String module;
     private String frequency;
+    private boolean active;
 
     private List<ChecklistItemTemplateDTO> items;
 }
