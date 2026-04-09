bcb3a952a3eaa193fde1b379672c3ce4499a8a24
Edvard Klavenes <edvardklavenes@gmail.com>
2026-04-09 20:44:08 +0200

    feat: creates inspection interface

--- frontend/src/interfaces/Inspection.interface.ts
+++ frontend/src/interfaces/Inspection.interface.ts
@@ -9,3 +9,33 @@ export interface InspectionLog {
   actor?: string
   timestamp: string
 }
+
+export interface InspectionStats {
+  // Deviations
+  deviationTotal: number
+  deviationOpen: number
+  deviationInProgress: number
+  deviationResolved: number
+  deviationCritical: number
+  deviationHigh: number
+  deviationMedium: number
+  deviationLow: number
+  // Temperature
+  temperatureTotal: number
+  temperatureOk: number
+  temperatureWarning: number
+  temperatureCritical: number
+  // Alcohol
+  alcoholTotal: number
+  alcoholOk: number
+  alcoholWarning: number
+  alcoholCritical: number
+}
+
+export interface InspectionExportFilter {
+  types?: string[]
+  severities?: string[]
+  statuses?: string[]
+  dateFrom?: string
+  dateTo?: string
+}
